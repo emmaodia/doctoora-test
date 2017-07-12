640b6f249c5578262fe45a1aaf6f61cf7d3d203f
Chidumaga <chidumaga@gmail.com>
2017-07-12 19:50:39 +0100

    adds doctors image to sidebar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712181511) do
+ActiveRecord::Schema.define(version: 20170712183828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,10 @@ ActiveRecord::Schema.define(version: 20170712181511) do
     t.string   "id_proof_content_type"
     t.integer  "id_proof_file_size"
     t.datetime "id_proof_updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "doctors", ["email"], name: "index_doctors_on_email", unique: true, using: :btree
