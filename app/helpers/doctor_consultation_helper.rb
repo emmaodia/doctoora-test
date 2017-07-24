27127792e97f665cd20362bed49352bf472fece4
Chidumaga <chidumaga@gmail.com>
2017-07-25 00:22:03 +0100

    adds viewable appointments in calendar

--- app/helpers/doctor_consultation_helper.rb
+++ app/helpers/doctor_consultation_helper.rb
@@ -5,12 +5,16 @@ module DoctorConsultationHelper
 		return patient.first_name + " " + patient.last_name
 	end
 
-
 	def my_profile?
 		if !current_doctor
 			return false
 		end
 		return current_doctor.id == params[:id].to_i
+	end
+
+	def get_consultation_details consultation_id
+		consultation = Consultation.find(consultation_id)
+		return [consultation.date, consultation.time]
 	end
 
 end
