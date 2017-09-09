8c607f57b4e975185143aa4a91fc8515c882fe7b
Chidumaga <chidumaga@gmail.com>
2017-09-09 03:17:39 +0100

    doctors verification tested and working

--- app/controllers/doctor_sessions_controller.rb
+++ app/controllers/doctor_sessions_controller.rb
@@ -1,5 +1,9 @@
 class DoctorSessionsController < Devise::SessionsController
 	def after_sign_in_path_for(resource_or_scope)
- 		doctors_path
+ 		if resource.verified == true && resource.town == nil
+ 			new_doctor_onboarding_path
+ 		else
+ 			root_path
+ 		end
 	end
 end
