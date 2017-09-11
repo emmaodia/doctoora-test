918ae86a12d8ba2847825762df985877d6025020
Chidumaga <chidumaga@gmail.com>
2017-09-11 16:47:45 +0100

    adds notification sidebar and slider to doctors

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,6 @@
 class HomeController < ApplicationController
 
 	def index
-		if user_signed_in?
-			@notifications = current_user.notifications.last(5).reverse
-		end
 	end
 	
 	def knowledgebase
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -19,6 +19,7 @@ class Doctor < ActiveRecord::Base
   has_many :consultations
   has_many :messages, as: :messageable
   has_many :patient_reviews
+  has_many :notifications
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
