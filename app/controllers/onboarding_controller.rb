86d7632073308a40781acdcbd26fc411548b90ca
Chidumaga <chidumaga@gmail.com>
2017-11-04 13:29:40 +0000

    rewrites onboarding controller

--- app/controllers/onboarding_controller.rb
+++ app/controllers/onboarding_controller.rb
@@ -5,28 +5,10 @@ class OnboardingController < ApplicationController
 	end
 
 	def create
-		user = User.find(current_user.id)
-		parameters = params["user"]
-
-		#save dob as an array and remove it from the params to be iterated over
-		user.dob = Date.civil(parameters["dob(1i)"].to_i,parameters["dob(2i)"].to_i,parameters["dob(3i)"].to_i)
-
-		#get the dob out of the way for iteration
-		(0..2).each do
-			parameters.shift
-		end
-
-		#calculate BMI
-		user.bmi = ((parameters["weight"].to_i/parameters["height"].to_f)/parameters["height"].to_f)
-
-		assign_values parameters, user
-
-		if user.save
-			redirect_to onboarding_new_additional_info_path
-		else
-			flash[:notice] = "There was an error saving your information"
-			render 'new'
-		end
+		@user = User.find(current_user)
+		@user.bmi = ((onboarding_params[:weight].to_i/onboarding_params[:height].to_f)/onboarding_params[:height].to_f)
+    	@user.update(onboarding_params)
+    	redirect_to onboarding_new_additional_info_path
 	end
 
 	def additional_info
@@ -34,24 +16,23 @@ class OnboardingController < ApplicationController
 	end
 
 	def submit_additional_info
-		user = User.find(current_user.id)
-		assign_values params["user"], user
-
-		if user.save
-			redirect_to root_path
-		else
-			flash[:notice] = "There was an error saving your information"
-			render 'additional_info'
-		end
+		@user = User.find(current_user.id)
+		@user.update(additional_params)
+		flash[:notice] = "Welcome to Doctoora. You have been onboarded successfully."
+		redirect_to root_path
 	end
 
 	private
+	
+	def onboarding_params
+		params.require(:user).permit(:dob, :gender, :ethnicity, :phone, :house, :town, :postcode, :country,
+									 :height, :weight, :bmi, :vaccinations, :avatar, :allergies, :medication,
+									 :lasting_conditions)
+	end
 
-	def assign_values params, user
-		params.each do |param|
-			key = param[0]
-			user[key] = param[1]
-		end
+	def additional_params
+		params.require(:user).permit(:allergies, :medication, :lasting_conditions, :recreational_drug,
+									:dietary_restrictions, :alcohol_consumption, :tobacco, :exercise)
 	end
 	
 end
