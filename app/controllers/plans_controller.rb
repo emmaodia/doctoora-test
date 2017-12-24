957bcc6df089b643afa1ed9d06e6f81aab212311
Chidumaga <chidumaga@gmail.com>
2017-12-24 16:44:11 +0100

    adds clinic rental date and time

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -24,6 +24,8 @@ class PlansController < ApplicationController
 		make_payment_dr clinic_rental_params[:payment_method], clinic.rental_cost, current_doctor.id, "transaction"
 
 		@clinic_rental = current_doctor.clinic_rentals.new(clinic_rental_params)
+		@clinic_rental.date_and_time = Time.new(@clinic_rental.date.year, @clinic_rental.date.month, @clinic_rental.date.day,
+  									   			   @clinic_rental.time.hour, @clinic_rental.time.min).to_datetime
 		@clinic_rental.clinic_id = clinic.id
 
 		if @clinic_rental.save
@@ -53,11 +55,11 @@ class PlansController < ApplicationController
 
 		user = User.find(current_user.id)
 
-		if transaction.purpose == "consultation"
-			#redirected here from consultation_controller#payment
-			flash[:notice] = "Thank you. Your consultation request has been sent and will be verified"
-			notify! current_user.id, transaction.doctor_id, "You have requested a consultation with",
-			"You have received a new consultation request from", "/consultation", "/doctor_consultation"
+		if transaction.purpose == "clinic_rental"
+			#redirected here from clinic_rental_controller#payment
+			flash[:notice] = "Thank you. Your clinic_rental request has been sent and will be verified"
+			notify! current_user.id, transaction.doctor_id, "You have requested a clinic_rental with",
+			"You have received a new clinic_rental request from", "/clinic_rental", "/doctor_clinic_rental"
 		elsif transaction.purpose == "purchase"
 			user.plan_id = transaction.plan_id
 			user.save
